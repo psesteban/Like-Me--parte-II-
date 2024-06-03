deb5e9969147de0c239daa0c136997dc402c07cf
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-06-03 12:09:31 -0400

    subiendo nuevo frontend

---
+++ server/src/controllers/likes.js
@@ -0,0 +1,19 @@
+import * as sql from '../models/posts.js'
+
+// GET /todos
+export const getPosts = (req, res) => sql.findAll()
+.then((result) => res.status(200).json({posts: result}))
+.catch ((error) => res.status(500).json({status: false, code: 500, message: error})
+)
+
+export const postPost = (req, res) => sql
+.createPost(req.body.titulo, req.body.url, req.body.descripcion)
+.then((result) => res.status(200).json({status: true, code: 200, message: result}))
+.catch ((error) => res.status(500).json({status: false, code: 500, message: error})
+)
+
+export const putPost = (req, res) => sql
+.createPost(req.body.titulo, req.body.url, req.body.descripcion)
+.then((result) => res.status(200).json({status: true, code: 200, message: result}))
+.catch ((error) => res.status(500).json({status: false, code: 500, message: error})
+)
